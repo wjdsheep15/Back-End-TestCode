342ff90d1aced053f36c00a79b4fcd2cdd26fbfe
wjdsheep15 <wjd15sheep@gmail.com>
2024-06-25 12:09:44 +0900

    [CHORE]-Add annotation

--- src/test/java/com/springboot/test/controller/ProductControllerTest.java
+++ src/test/java/com/springboot/test/controller/ProductControllerTest.java
@@ -33,9 +33,9 @@ public class ProductControllerTest {
 
     @Test
     @DisplayName("MockMvc를 통한 Product 데이터 가져오기 테스트")
-    void getProduct() throws Exception {
+    void createProductTest() throws Exception {
         //Mock 객체에서 특정 메서드가 실행되는 경우 실제 Return을 줄 수 없기 때문에 아래와 같이 가정 사항을 만들어줌
-
+        // Test에 필요하나 객체를 생성
         given(productService.saveProduct(new ProductDto("pen", 5000, 2000)))
                 .willReturn(new ProductResponseDto(12315L, "pen", 5000, 2000));
 
@@ -44,7 +44,7 @@ public class ProductControllerTest {
                 .price(5000)
                 .stock(2000)
                 .build();
-
+        // 리소스 생성 기능을 테스트하기 때문에 post 메서드를 통해 URL을 구성, 마지막으로 POST 요청을 통해 도출된 결과값에 대해 각 항목에 존재하는지 jsonPath().exists()를 통해 검증
         Gson gson = new Gson();
         String content = gson.toJson(productDto);
 
